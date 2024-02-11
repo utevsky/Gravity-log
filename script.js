21250e81d68ee9e4ca9a4032b9f901e04033c0fa
utevsky <159273510+utevsky@users.noreply.github.com>
2024-02-11 13:55:50 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+const button = document.querySelector("button");
+const div = document.querySelector(".content");
+
+button.addEventListener("click", function () {
+
+    html2canvas(div).then((canvas) => {
+        const imageDataURL = canvas.toDataURL("image/png");
+
+        // Create a download link for the image
+        const a = document.createElement("a");
+        a.href = imageDataURL;
+        a.download = "image.png";
+        a.click();
+
+    });
+
+});
